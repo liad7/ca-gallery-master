0ca143d278e152e92f4f975de649586599d13e76
liad7 <liadgola7@gmail.com>
2022-12-05 17:05:20 +0200

    protfilo render

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-console.log('Starting up');
+'use strict'
+
+$(document).ready(init)
+
+function init(){
+    console.log('Starting up');
+    renderProtfolio()
+}
+
+function renderProtfolio(){
+    const projs = getProjs()
+    var strHTMLs = projs.map(proj => 
+        `<div class="col-md-4 col-sm-6 portfolio-item">
+          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+            <div class="portfolio-hover">
+              <div class="portfolio-hover-content">
+                <i class="fa fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img class="img-fluid" src="img/portfolio/${proj.id}.png" alt="">
+          </a>
+          <div class="portfolio-caption">
+            <h4>${proj.name}</h4>
+            <p class="text-muted">${proj.title}</p>
+          </div>
+        </div>`
+    )
+    console.log(strHTMLs);
+        $('.port-content').html(strHTMLs)
+
+}
+
---
+++ js/service.js
@@ -0,0 +1,68 @@
+'use strict'
+
+var gProjs = [
+    {
+    id: "mine-sweeper",
+    name: "Mine-Sweeper",
+    title: "Try Not To Bomb",
+    desc: "lorem ipsum lorem ipsum lorem ipsum", url: "https://liad7.github.io/mine-sweeper1/",
+    publishedAt: 1448693940000,
+    labels: ["Matrixes", "keyboard events"]
+},
+{
+    id: "touch-nums",
+    name: "Touch-Nums",
+    title: "Beat The Clock",
+    desc: "lorem ipsum lorem ipsum lorem ipsum", url: "https://liad7.github.io/Touch-Nums/",
+    publishedAt: 1448693940000,
+    labels: ["Matrixes"]
+},
+{
+    id: "in-picture",
+    name: "In-Picture",
+    title: "Fun Actors Quiz",
+    desc: "lorem ipsum lorem ipsum lorem ipsum", url: "https://liad7.github.io/In-Picture/",
+    publishedAt: 1448693940000,
+    labels: []
+},
+{
+    id: "chess",
+    name: "Chess",
+    title: "Kings\' Game",
+    desc: "lorem ipsum lorem ipsum lorem ipsum", url: " https://liad7.github.io/Chess/",
+    publishedAt: 1448693940000,
+    labels: ["Matrixes"]
+},
+{
+    id: "ball-game",
+    name: "Ball-Game",
+    title: "Collects The Balls",
+    desc: "lorem ipsum lorem ipsum lorem ipsum", url: "https://liad7.github.io/Ball-Game/",
+    publishedAt: 1448693940000,
+    labels: ["Matrixes", "keyboard events"]
+},
+{
+    id: "pacman",
+    name: "Pacman",
+    title: "Mind The Ghosts",
+    desc: "lorem ipsum lorem ipsum lorem ipsum", url: "https://liad7.github.io/Pacman/",
+    publishedAt: 1448693940000,
+    labels: ["Matrixes", "keyboard events"]
+},
+]
+
+function getProjs(){
+    return gProjs
+}
+
+
+// function _createProj(id,name,title,desc,publishedAt,labels){
+//     return {
+//         id,
+//         name,
+//         title,
+//         desc,
+//         publishedAt,
+//         labels,
+//     }
+// }
